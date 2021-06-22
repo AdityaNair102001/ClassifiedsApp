2a91fad86c1beaa7b9bc0e8d9f1f3f36cd164023
AdityaNair102001 <adityanair102001@gmail.com>
2021-06-22 17:27:31 +0530

    Feed Implemented.

---
+++ app/src/main/java/com/phoenixcorp/classifiedsapp/FeedListAdapter.java
@@ -0,0 +1,87 @@
+package com.phoenixcorp.classifiedsapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FeedListAdapter extends RecyclerView.Adapter<FeedListAdapter.FeedListViewHolder> {
+    ArrayList<String> products;
+    ArrayList<String> prices;
+    ArrayList<String> UIDs;
+
+    HashMap<String,String> names;
+    HashMap<String,String> imageURLs;
+
+    Fragment homeFragment;
+
+    public FeedListAdapter( ArrayList<String>products,ArrayList<String> prices, HashMap<String,String> imageURLs, ArrayList<String> UIDs,  HashMap<String,String> names,HomeFragment homeFragment) {
+        this.products=products;
+        this.prices=prices;
+        this.imageURLs=imageURLs;
+        this.names=names;
+        this.UIDs=UIDs;
+        this.homeFragment=homeFragment;
+    }
+
+    @NonNull
+    @Override
+    public FeedListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater =  LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.feedlist_feed_layout,parent,false);
+        return new FeedListViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedListViewHolder holder, int position) {
+        String productName=products.get(position);
+        String price= "\u20B9"+ prices.get(position);
+        String url=imageURLs.get(productName);
+        holder.productName.setText(productName);
+        holder.price.setText(price);
+        Picasso.get().load(url).into(holder.feedImage);
+
+        holder.productCard.setOnClickListener(v -> {
+            Intent intent = new Intent(homeFragment.getActivity(), ProductDescription.class);
+            homeFragment.startActivity(intent);
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class FeedListViewHolder extends RecyclerView.ViewHolder{
+
+        TextView productName;
+        TextView price;
+        ImageView feedImage;
+        CardView productCard;
+
+        public FeedListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName=itemView.findViewById(R.id.productNameTextView);
+            price=itemView.findViewById(R.id.priceTextView);
+            feedImage=itemView.findViewById(R.id.feedImage);
+            productCard= itemView.findViewById(R.id.productCard);
+
+        }
+    }
+}
--- app/src/main/java/com/phoenixcorp/classifiedsapp/HomeFragment.java
+++ app/src/main/java/com/phoenixcorp/classifiedsapp/HomeFragment.java
@@ -1,14 +1,31 @@
 package com.phoenixcorp.classifiedsapp;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,7 +81,108 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView feed=view.findViewById(R.id.feed);
+
+
+
+        ArrayList<String> productsFromDB=new ArrayList<>();
+        ArrayList<String> priceFromDB=new ArrayList<>();
+        ArrayList<String> UIDFromDB=new ArrayList<>();
+        HashMap<String,String> names=new HashMap<>();
+
+        HashMap<String,String> imageURLFromDB=new HashMap<>();
+
+
+        FeedListAdapter adapter;
+
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document:task.getResult()){
+                        productsFromDB.add(document.getString("productName"));
+                        priceFromDB.add(document.getString("price"));
+                        UIDFromDB.add(document.getString("UID"));
+
+                        db.collection("posts/"+document.getId()+"/urls").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                List<DocumentSnapshot> documentList=task.getResult().getDocuments();
+
+
+
+                                for(int i=0;i<1;i++){
+//                                    imageURLFromDB.add(documentList.get(i).getString("url"));
+                                    imageURLFromDB.put(document.getString("productName"),documentList.get(i).getString("url"));
+                                }
+
+                                adapterHandler(productsFromDB,priceFromDB,imageURLFromDB,UIDFromDB,names,feed);
+                                feed.setHasFixedSize(true);
+
+                            }
+                        });
+                        Log.d("products", "onCreateView: images form db "+imageURLFromDB);
+                    }
+//                    adapter=new FeedListAdapter(productsFromDB,prices);
+
+
+                    Log.d("products", "onComplete:106 "+productsFromDB);
+
+                }else{
+                    Toast.makeText(getContext(),"Couldn't Fetch",Toast.LENGTH_LONG).show();
+                }
+            }
+
+
+        });
+
+        for(String uid:UIDFromDB) {
+            db.collection("users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                    DocumentSnapshot user=task.getResult();
+                    String name=user.getString("username");
+
+                    names.put(uid,name);
+
+
+                }
+            });
+        }
+
+
+
+
+        feed.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false));
+//        feed.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+        return view;
     }
+
+    private void adapterHandler(ArrayList<String> products,ArrayList<String> prices, HashMap<String,String> imagesURLs, ArrayList<String> UIDs,HashMap<String,String> names,RecyclerView feed) {
+        FeedListAdapter adapter=new FeedListAdapter(products,prices,imagesURLs,UIDs,names,this);
+        if(imagesURLs.size()!=products.size() && names.size()!=products.size()){
+            return;
+        }else{
+            Log.d("products", "onCreateView: "+imagesURLs);
+            feed.setAdapter(adapter);
+        }
+
+        Log.d("images", "adapterHandler: at 131"+imagesURLs.size());
+    }
+
+
 }

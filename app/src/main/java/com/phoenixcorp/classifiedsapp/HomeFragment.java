c0e2a4fdb19394d0aa2f643f12bbc25483f8ccb2
AdityaNair102001 <adityanair102001@gmail.com>
2021-06-25 11:55:42 +0530

    Like post feature implemented.

--- app/src/main/java/com/phoenixcorp/classifiedsapp/HomeFragment.java
+++ app/src/main/java/com/phoenixcorp/classifiedsapp/HomeFragment.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -30,6 +31,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -99,51 +101,58 @@ public class HomeFragment extends Fragment {
         ArrayList<String> UIDFromDB=new ArrayList<>();
         ArrayList<String> documentID = new ArrayList<>();
         ArrayList<String> location=new ArrayList<>();
+        ArrayList<String> myAds=new ArrayList<>();
+
+        HashMap<String,Boolean> likedPostsFromDB = new HashMap<>();
 
         HashMap<String,String> names=new HashMap<>();
 
         HashMap<String,String> imageURLFromDB=new HashMap<>();
 
+        final String currentUser = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
-        FeedListAdapter adapter;
+
 
         FirebaseFirestore db=FirebaseFirestore.getInstance();
         db.collection("posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
-                    for(QueryDocumentSnapshot document:task.getResult()){
-                        productsFromDB.add(document.getString("productName"));
-                        priceFromDB.add(document.getString("price"));
-                        UIDFromDB.add(document.getString("UID"));
-                        documentID.add(document.getId());
-                        location.add(document.getString("location"));
+                    for(QueryDocumentSnapshot document: Objects.requireNonNull(task.getResult())){
+                        if(!Objects.equals(document.getString("UID"), currentUser)){
+                            productsFromDB.add(document.getString("productName"));
+                            priceFromDB.add(document.getString("price"));
+                            UIDFromDB.add(document.getString("UID"));
+                            documentID.add(document.getId());
+                            location.add(document.getString("location"));
 
-                        db.collection("posts/"+document.getId()+"/urls").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            db.collection("posts/"+document.getId()+"/urls").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
-                                List<DocumentSnapshot> documentList=task.getResult().getDocuments();
+                                    List<DocumentSnapshot> documentList=task.getResult().getDocuments();
 
 
 
-                                for(int i=0;i<1;i++){
-//                                    imageURLFromDB.add(documentList.get(i).getString("url"));
-                                    imageURLFromDB.put(document.getId(),documentList.get(i).getString("url"));
+                                    for(int i=0;i<1;i++){
+
+                                        imageURLFromDB.put(document.getId(),documentList.get(i).getString("url"));
+                                    }
+
+                                    adapterHandler(productsFromDB,priceFromDB,imageURLFromDB,UIDFromDB,location,names,feed,progressBar,documentID,likedPostsFromDB);
+
+                                    feed.setHasFixedSize(true);
+
                                 }
+                            });
 
-                                adapterHandler(productsFromDB,priceFromDB,imageURLFromDB,UIDFromDB,location,names,feed,progressBar,documentID);
+                        }else{
+                            myAds.add(document.getId());
+                        }
 
-                                feed.setHasFixedSize(true);
-
-                            }
-                        });
-                        Log.d("products", "onCreateView: images form db "+imageURLFromDB);
                     }
-//                    adapter=new FeedListAdapter(productsFromDB,prices);
 
 
-                    Log.d("products", "onComplete:106 "+productsFromDB);
 
                 }else{
                     Toast.makeText(getContext(),"Couldn't Fetch",Toast.LENGTH_LONG).show();
@@ -159,6 +168,7 @@ public class HomeFragment extends Fragment {
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
                     DocumentSnapshot user=task.getResult();
+                    assert user != null;
                     String name=user.getString("username");
 
                     names.put(uid,name);
@@ -168,27 +178,24 @@ public class HomeFragment extends Fragment {
             });
         }
 
-
-
-
-        feed.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false));
-//        feed.setAdapter(adapter);
-
-
-
-
-
-
-
-
+        db.collection("users").document(currentUser).collection("liked posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                   for(DocumentSnapshot documentSnapshot: Objects.requireNonNull(task.getResult())){
+                       likedPostsFromDB.put(documentSnapshot.getId(),true);
+                   }
+                }
+            }
+        });
 
         return view;
     }
 
     private void adapterHandler(ArrayList<String> products,ArrayList<String> prices, HashMap<String,String> imagesURLs, ArrayList<String> UIDs,
-                                ArrayList<String> location, HashMap<String,String> names,RecyclerView feed,CircularProgressIndicator progressBar,ArrayList<String> documentID) {
+                                ArrayList<String> location, HashMap<String,String> names,RecyclerView feed,CircularProgressIndicator progressBar,ArrayList<String> documentID,HashMap<String,Boolean> likedPosts) {
 
-        FeedListAdapter adapter=new FeedListAdapter(products,prices,imagesURLs,UIDs,location,names,this,documentID);
+        FeedListAdapter adapter=new FeedListAdapter(products,prices,imagesURLs,UIDs,location,names,this,documentID,likedPosts);
 
         if(imagesURLs.size()!=products.size() && names.size()!=products.size()){
             return;
@@ -200,8 +207,6 @@ public class HomeFragment extends Fragment {
             Collections.reverse(location);
             Collections.reverse(documentID);
 
-
-            Log.d("products", "onCreateView:173 "+imagesURLs);
             progressBar.setVisibility(View.INVISIBLE);
             feed.setLayoutManager(new GridLayoutManager(this.getContext(),2));
             feed.setItemViewCacheSize(20);
@@ -209,7 +214,6 @@ public class HomeFragment extends Fragment {
             feed.setAdapter(adapter);
         }
 
-        Log.d("images", "adapterHandler: at 131"+imagesURLs.size());
     }
 
 
